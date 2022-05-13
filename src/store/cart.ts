00c8d2c5b334efe25b0a856c0dc491d7162e09f0
Chanyoung Park <rhdwkrxla68@gmail.com>
2022-05-13 18:42:46 +0900

    refactor: 주석 추가

--- src/store/cart.ts
+++ src/store/cart.ts
@@ -1,29 +1,42 @@
 import { defineStore } from 'pinia';
 
-export type StateType = {
-  items: ItemType[];
-};
-
 export type ItemType = {
   name: string;
   price?: number;
   count?: number;
 };
 
+export type StateType = {
+  items: ItemType[];
+};
+
 export const useCart = defineStore('cart', () => {
   const items: ItemType[] = reactive([]);
+  /**
+   * items 배열에 item을 추가합니다.
+   * @param item
+   */
   const addItem = (item: ItemType) => {
     items.push(item);
   };
+  /**
+   * items 배열에 들어있는 item을 name을 기준으로 변경합니다.
+   * count 프로퍼티는 예외적으로 기존 값에 추가됩니다.
+   * @param name
+   * @param item
+   * @returns void
+   */
   const updateByName = (name: string, item: ItemType) => {
-    const index = items.findIndex((i) => i.name === item.name);
-    if (index <= -1) return;
+    const index = items.findIndex(i => i.name === name);
+    if (index <= -1) {
+      return;
+    }
     const original = items[index];
     items.splice(index, 1, { ...original, ...item, count: original.count + item.count });
   };
   return {
     items,
     addItem,
-    updateByName,
+    updateByName
   };
 });
